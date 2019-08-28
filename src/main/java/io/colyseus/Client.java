606019f614a06d7073644f5963949a416df25fa5
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-28 07:57:38 +0430

    fix body not being sent when no option is set (in match maker http request)

--- src/main/java/io/colyseus/Client.java
+++ src/main/java/io/colyseus/Client.java
@@ -122,10 +122,10 @@ public class Client {
         try {
             String url = endpoint.replace("ws", "http") + "/matchmake/" + method + "/" + URLEncoder.encode(roomName, "UTF-8");
 
-            String body = null;
+            String body;
             if (options != null) {
                 body = objectMapper.writeValueAsString(objectMapper.convertValue(options, JsonNode.class));
-            }
+            } else body = "{}";
 
             if (httpHeaders == null) httpHeaders = new LinkedHashMap<>();
 
