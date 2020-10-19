31fd6c4d67af474f79fe447edb8e1d9a76a27030
Milad Doorbash <milad.doorbash@gmail.com>
2020-10-19 07:47:48 +0330

    Add ArraySchema test

---
+++ src/test/kotlin/io/colyseus/test/schema/ArraySchemaTest.kt
@@ -0,0 +1,41 @@
+package io.colyseus.test.schema
+
+import io.colyseus.serializer.schema.types.ArraySchema
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ArraySchemaTest {
+    @Test
+    fun `ArraySchema test`() {
+        fun doSomethingWithArray(array: ArrayList<Int>): ArrayList<Int> {
+            return array.apply {
+                add(3)
+                add(4000)
+                add(-100)
+                removeAt(1)
+                add(0)
+                set(0, 200)
+                remove(200)
+                set(1, 300)
+                removeAt(0)
+                add(-100)
+                add(400)
+            }
+        }
+
+        val arrayList = doSomethingWithArray(ArrayList())
+        val arraySchema = doSomethingWithArray(ArraySchema<Int>() as ArrayList<Int>)
+
+        println(arrayList)
+
+        assertEquals(arrayList.size, arraySchema.size)
+
+        arraySchema.forEachIndexed { index, element ->
+            assertEquals(element, arrayList[index])
+        }
+
+        repeat(arrayList.size) {
+            assertEquals(arraySchema[it], arrayList[it])
+        }
+    }
+}
